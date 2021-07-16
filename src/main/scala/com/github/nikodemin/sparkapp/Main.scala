44c1745065004ac31b6bc6b24396e7909957788d
Nikita Demin <niko.demin@gmail.com>
2021-07-16 07:56:38 +0300

    completed tasks

--- src/main/scala/com/github/nikodemin/sparkapp/Main.scala
+++ src/main/scala/com/github/nikodemin/sparkapp/Main.scala
@@ -1,6 +1,9 @@
 package com.github.nikodemin.sparkapp
 
+import java.sql.Date
+
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 object Main {
@@ -38,9 +41,44 @@ object Main {
       .schema(reservationSchema)
       .csv("src/main/resources/reservation_dataset.csv")
 
-    merchantDf.printSchema
-    merchantDf.show
-    reservationDf.printSchema
-    reservationDf.show
+    val emailVerification = udf((email: String) => email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"))
+
+    val validatedReservationDf = reservationDf.filter(emailVerification(col("merchant_email"))).cache
+
+    println(s"Reservations count: ${reservationDf.count}, valid: ${validatedReservationDf.count}," +
+      s" invalid: ${reservationDf.count - validatedReservationDf.count}")
+
+    validatedReservationDf.agg(avg("guest_count")).show
+
+    println("Merchant with top guests count:")
+    validatedReservationDf.filter(col("guest_count").gt(1))
+      .groupBy("merchant_id")
+      .agg(sum("guest_count").as("guests_sum"))
+      .orderBy(col("guests_sum").desc)
+      .limit(1)
+      .join(merchantDf, "merchant_id")
+      .show
+
+    val quarters = Vector("January, February, March", "April, May, June", "July, August, September",
+      "October, November, December")
+    val getQuarter = udf((date: Date) => quarters((date.toLocalDate.getMonthValue - 1) / 3))
+
+    val reservationsByQuartersDf = validatedReservationDf
+      .groupBy(getQuarter(col("created_at")).as("quarter"), col("merchant_id"))
+      .agg(count("*").as("reservations"))
+      .cache
+
+    println("Merchants with top guests count by quarters:")
+    reservationsByQuartersDf.as("a").join(reservationsByQuartersDf.as("b"),
+      when(expr("a.quarter = b.quarter AND a.reservations < b.reservations"), true)
+        .otherwise(false)
+      , "left")
+      .where("b.reservations is NULL")
+      .select("a.*")
+      .join(merchantDf, "merchant_id")
+      .show
+
+    validatedReservationDf.unpersist(true)
+    reservationsByQuartersDf.unpersist(true)
   }
 }
